b1c279e95d9c08c15e3e431656e57d496a28145b
Guillaume Martigny <guillaume.martigny@gmail.com>
2016-02-12 11:53:23 +0100

    Revert to not using Angular Signed-off-by:Guillaume Martigny <guillaume.martigny@gmail.com>

--- public_html/depth.js
+++ public_html/depth.js
@@ -1,12 +1,27 @@
-window.onload = function(){
+(function(){
     "use strict";
     var main = document.getElementById("main"),
-        articles = main.getElementsByTagName("article"),
+        articles = main.getElementsByClassName("article"),
         afix = document.getElementById("afix"),
         afix_active = null,
         depth = 0;
 
-//    document.body.classList.add("ready");
+    for(var i=0, l=articles.length; i < l; ++i){
+        var article = articles[i],
+            li = document.createElement("li");
+
+        li.innerHTML = article.getElementsByTagName("h1")[0].innerHTML;
+        li.addEventListener("click", (function(dest){
+            return function(){
+                jumpTo(dest);
+            };
+        })(i));
+
+        afix.appendChild(li);
+
+        article.style.zIndex = l - i;
+        article.style.transitionDelay = "0s, 0." + (l - i) + "s";
+    }
 
     window.addEventListener("wheel", function(e){
         if(!loopTimer){
@@ -15,10 +30,33 @@ window.onload = function(){
                 depth = 0;
             else if(depth > articles.length - 1)
                 depth = articles.length - 1;
-//            updateAfix();
+            simulateDepth();
+            updateAfix();
         }
     });
 
+    updateAfix();
+    simulateDepth();
+    setTimeout(function(){
+        document.body.classList.add("ready");
+    }, 0);
+
+    function simulateDepth(){
+        for(var i = 0, l = articles.length; i < l; ++i){
+            var article = articles[i],
+                scale = "scale(" + (1 / Math.pow(1.7, (i - depth))) + ")",
+                blur = "blur(" + ((i - depth) * 2) + "px)";
+            article.style.transform = scale;
+            article.style.WebkitTransform = scale;
+            article.style.filter = blur;
+            article.style.WebkitFilter = blur;
+
+            if(depth-0.7 > i)
+                article.classList.add("overflow");
+            else
+                article.classList.remove("overflow");
+        }
+    }
 
     var loopTimer = 0;
     function jumpTo(dest){
@@ -26,22 +64,26 @@ window.onload = function(){
             clearTimeout(loopTimer);
 
         var diff = parseFloat(((dest - depth) / 10).toFixed(3));
-        depth += diff;
 
         if(diff){
+            depth += diff;
             loopTimer = setTimeout(function(){
                 loopTimer = 0;
                 jumpTo(dest);
             }, 16);
         }
-//        updateAfix();
+        else{
+            depth = dest;
+        }
+        updateAfix();
+        simulateDepth();
     }
 
-//    function updateAfix(){
-//        var lis = afix.getElementsByTagName("li");
-//        if(afix_active)
-//            afix_active.classList.remove("active");
-//        afix_active = lis[(depth + 0.5) << 0];
-//        lis[(depth + 0.5) << 0].classList.add("active");
-//    }
-};
+    function updateAfix(){
+        var lis = afix.getElementsByTagName("li");
+        if(afix_active)
+            afix_active.classList.remove("active");
+        afix_active = lis[(depth + 0.5) << 0];
+        lis[(depth + 0.5) << 0].classList.add("active");
+    }
+})();
