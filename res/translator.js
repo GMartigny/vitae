e925bd5f47dcb296cdcd846e6f11f8ede227bece
Guillaume Martigny <guillaume.martigny@gmail.com>
2016-03-17 16:56:22 +0100

    translator: set page lang, commenting, code formating css wait for translator Signed-off-by:Guillaume Martigny <guillaume.martigny@gmail.com>

--- res/translator.js
+++ res/translator.js
@@ -1,20 +1,22 @@
 (function(){
 
-    var DEFAULT_LANGUAGE = "en";
-
     var self = this;
 
+    // prepare html for translation
     var html = document.body.innerHTML;
     document.body.innerHTML = html.replace(/{{(.+?)}}/g, '<span class="translator" data-translator-key="$1"></span>');
 
-    this.data = {};
-    this.lang = DEFAULT_LANGUAGE;
+    // get lang from localStorage then from navigator and fallback to "en"
+    this.lang = localStorage.getItem("lang") || navigator.language.split("-")[0] || "en";
 
+    this.data = {};
+
+    // AJAX request for data
     var request = new XMLHttpRequest();
     request.onload = function(){
         try{
             self.data = JSON.parse(this.responseText);
-            self.translate();
+            self.translateTo();
         }
         catch(e){
             console.log("Impossible to get i18n data");
@@ -23,41 +25,47 @@
     request.open("GET", "data.json", true);
     request.send();
 
+    // activate switches
     var switchs = document.getElementsByClassName("translator-switch"),
     i = switchs.length, el;
-
     while(el = switchs[--i]){
         el.addEventListener("click", function(){
-            self.lang = this.dataset.lang;
-            self.translate();
-            self.activeSwitch.classList.remove("active");
-            self.activeSwitch = this;
+            self.translateTo(this.dataset.lang);
+            document.getElementsByClassName("translator-switch active")[0].classList.remove("active");
             this.classList.add("active");
         });
-        if(el.dataset.lang == DEFAULT_LANGUAGE){
+        if(el.dataset.lang == this.lang){
             el.classList.add("active");
-            self.activeSwitch = el;
         }
     }
 
-    this.translate = function(){
+    /**
+     * Update page to new language
+     * @param {String} lang The selected language
+     * @returns {Translator} Itself
+     */
+    this.translateTo = function(lang){
+        var lang = lang || this.lang;
         document.body.classList.remove("translator-ready");
 
-        var strings = document.getElementsByClassName("translator"),
-        i = strings.length, el, str;
+        this.lang = lang;
+        document.documentElement.lang = lang;
+        localStorage.setItem("lang", lang);
 
+        var strings = document.getElementsByClassName("translator"),
+            i = strings.length,
+            data = this.data[lang], el, str;
+
+        // crawl page
         while(el = strings[--i]){
-            str = this.data[this.lang][el.dataset.translatorKey];
-            if(str){
-                if(str instanceof Array)
-                    str = str.join("<br/>");
-                el.innerHTML = str;
-            }
-            else{
-                el.innerHTML = el.dataset.translatorKey;
-            }
+            str = data[el.dataset.translatorKey];
+
+            if(str instanceof Array)
+                str = str.join("<br/>");
+            el.innerHTML = str? str: el.dataset.translatorKey;
         }
 
         document.body.classList.add("translator-ready");
+        return this;
     };
 })();
